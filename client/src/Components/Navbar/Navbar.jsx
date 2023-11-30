dde5444b5c2642768e8adcf7dcc3ffdcd2471131
maxi-pereyra <maxiabelpereyra@hotmail.com.ar>
2023-11-30 04:24:40 -0300

    update

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 //mport style from './Navbar.module.css';
 import   Nav   from "react-bootstrap/Nav" ;
 import  Navbar from 'react-bootstrap/Navbar';
-import  Container  from 'react-bootstrap/container';
+//import  Container  from 'react-bootstrap/container';
 import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navigation = ({onSearch,handleChange}) => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
+      {/* <Container> */}
           <Navbar.Brand href="/"> Dogs
             </Navbar.Brand>   
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -35,7 +35,7 @@ const Navigation = ({onSearch,handleChange}) => {
                 </Nav.Item>
               </Nav>
             </Navbar.Collapse>
-      </Container>
+      {/* </Container> */}
     </Navbar>
   )
 }
